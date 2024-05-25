7b3ad5656cd9060c2c5abddf574b21d0b5ba6d6e
adlansubejo <m.faliq.adlan@roketin.com>
2024-05-25 16:36:26 +0700

    edit input

--- main.py
+++ main.py
@@ -34,9 +34,9 @@ paramSymbolList = [m, k, l, g]
 
 # Define the values for the parameters
 mVal, kVal, lVal, gVal = (
-    1,
-    10,
-    1,
+    2,
+    8,
+    0.5,
     9.81,
 )  # These are just example values, replace with your actual values
 paramVal = [
@@ -47,14 +47,19 @@ paramVal = [
 ]
 
 # Define the time span for which to solve the equations
+t_range = 10  # Adjust time range as needed.
+num_points = 400  # Adjust number of points as needed.
 tSpan = np.linspace(
-    0, 10, 100
+    0, t_range, num_points
 )  # Solve the equations from t=0 to t=10 with 10 points in between
 
 # Define the initial conditions for the generalized coordinates and their derivatives
+r0 =  lVal + (mVal * gVal / kVal)  # Initial value of r, replace with the actual value.
+theta0 = np.pi / 10  # Initial value of θ, replace with the actual value.
+
 initCnd = [
-    np.pi / 10,
-    lVal + (mVal * gVal / kVal),
+    theta0,
+    r0,
     0,
     0,
 ]  # These are just example values, replace with your actual initial conditions
@@ -64,13 +69,12 @@ SS, xx = lagrangeSolver.DynamicEqSolver(Eq, paramSymbolList, paramVal, tSpan, in
 
 # compare with the exact solution
 # Constants
-A = 10 # Amplitude for r, replace with the actual value.
-B = np.pi / 10  # Amplitude for θ, replace with the actual value.
-r0 = 0  # Initial value of r, replace with the actual value.
+A = 0.05 * r0  # Amplitude for r, replace with the actual value.
+B = theta0  # Amplitude for θ, replace with the actual value.
+r0 = r0  # Initial value of r, replace with the actual value.
 phi_1 = 0  # Phase constant for r, replace with the actual value.
 phi_2 = 0  # Phase constant for θ, replace with the actual value.
-t_range = 10  # Adjust time range as needed.
-num_points = 100  # Adjust number of points as needed.
+
 
 motion = HarmonicMotion(
     lVal, mVal, gVal, kVal, A, B, r0, phi_1, phi_2, t_range, num_points
@@ -79,29 +83,29 @@ rExact = motion.calculate_r()
 thetaExact = motion.calculate_theta()
 
 # Plot the motion compared to the exact solution
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
-# plt.figure(figsize=(12, 6))
+plt.figure(figsize=(12, 6))
 
-# plt.subplot(2, 1, 1)
-# plt.plot(tSpan, xx[:, 1], label="Numerical")
-# plt.plot(motion.t, rExact, label="Exact")
-# plt.title("Plot of r against t")
-# plt.xlabel("Time (t)")
-# plt.ylabel("r(t)")
-# plt.legend()
+plt.subplot(2, 1, 1)
+plt.plot(tSpan, xx[:, 1], label="Numerical")
+plt.plot(motion.t, rExact, label="Exact")
+plt.title("Plot of r against t")
+plt.xlabel("Time (t)")
+plt.ylabel("r(t)")
+plt.legend()
 
-# plt.subplot(2, 1, 2)
-# plt.plot(tSpan, xx[:, 0], label="Numerical")
-# plt.plot(motion.t, thetaExact, label="Exact")
-# plt.title("Plot of θ against t")
-# plt.xlabel("Time (t)")
-# plt.ylabel("θ(t)")
-# plt.legend()
+plt.subplot(2, 1, 2)
+plt.plot(tSpan, xx[:, 0], label="Numerical")
+plt.plot(motion.t, thetaExact, label="Exact")
+plt.title("Plot of θ against t")
+plt.xlabel("Time (t)")
+plt.ylabel("θ(t)")
+plt.legend()
 
-# plt.tight_layout()
-# plt.show()
+plt.tight_layout()
+plt.show()
 
 # animate the motion
 # Create an instance of the Animator2 class
-animator = Animator2(xx, tSpan)
+# animator = Animator2(xx, tSpan)
